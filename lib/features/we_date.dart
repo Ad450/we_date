8a5f0254d558c0bccc3b0ffa1a8ba2afea1e4b43
Emmanuel <adjeimanue@gmail.com>
2023-07-04 02:48:59 +0000

    cleanup

--- lib/features/we_date.dart
+++ lib/features/we_date.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:we_date/core/di/configure_dependencies.dart';
-import 'package:we_date/features/auth/data/usecases/check_auth_status.dart';
-import 'package:we_date/features/auth/data/usecases/signup_or_login_with_fb.dart';
-import 'package:we_date/features/auth/data/usecases/signup_or_login_with_google.dart';
+import 'package:we_date/core/utils/state_providers.dart';
 import 'package:we_date/features/auth/screens/auth_landing_page.dart';
-import 'package:we_date/features/auth/state/auth_bloc.dart';
-import 'package:we_date/features/home/discover/data/usecases/fetch_discovered_profiles.dart';
-import 'package:we_date/features/home/discover/state/discover_bloc.dart';
 import 'package:we_date/features/home/home.dart';
-import 'package:we_date/features/home/stories/data/usecases/get_stories.dart';
-import 'package:we_date/features/home/stories/data/usecases/upload_story.dart';
-import 'package:we_date/features/home/stories/screens/state/stories_bloc.dart';
 import 'package:we_date/features/onboarding/screens/onboarding.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:we_date/features/profile/screens/state/profile_bloc.dart';
-
-import 'profile/data/usecases/update_profile.dart';
 
 class WeDate extends StatelessWidget {
   final bool? showAuthPage;
@@ -26,56 +14,29 @@ class WeDate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => AuthenticationBloc(
-              signupOrLoginWithFacebook: Injector.getIt.get<SignupOrLoginWithFacebook>(),
-              signupOrLoginWithGoogle: Injector.getIt.get<SignupOrLoginWithGoogle>(),
-              checkAuthStatus: Injector.getIt.get<CheckAuthStatus>()),
-        ),
-        BlocProvider(
-          create: (_) => DiscoverBloc(
-            fetchDiscoveredProfiles: Injector.getIt.get<FetchDiscoveredProfiles>(),
-          ),
-        ),
-        BlocProvider(
-          create: (_) => StoriesBloc(
-            uploadStory: Injector.getIt.get<UploadStory>(),
-            getStories: Injector.getIt.get<GetStories>(),
-          ),
-        ),
-        BlocProvider(
-          create: (_) => ProfileBloc(
-            updateProfile: Injector.getIt.get<UpdateProfile>(),
-          ),
-        ),
-      ],
-      child: StartWeDate(showAuthPage: showAuthPage, isAuthenticated: isAuthenticated),
+      providers: [...stateProviders],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'WeDate',
+        home: StartWeDate(showAuthPage: showAuthPage, isAuthenticated: isAuthenticated),
+      ),
     );
   }
 }
 
-class StartWeDate extends StatefulWidget {
+class StartWeDate extends StatelessWidget {
   final bool? showAuthPage;
   final bool? isAuthenticated;
 
   const StartWeDate({super.key, required this.showAuthPage, required this.isAuthenticated});
 
-  @override
-  State<StartWeDate> createState() => _StartWeDateState();
-}
-
-class _StartWeDateState extends State<StartWeDate> {
+  // @override
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'WeDate',
-      home: widget.isAuthenticated != null && widget.isAuthenticated!
-          ? const HomeScreen()
-          : widget.showAuthPage != null
-              ? const AuthLandingPage()
-              : const Onboarding(),
-    );
+    return isAuthenticated != null && isAuthenticated!
+        ? const HomeScreen()
+        : showAuthPage != null
+            ? const AuthLandingPage()
+            : const Onboarding();
   }
 }
